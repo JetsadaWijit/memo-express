120bef37a276533213143f1dbe0780393a99d2ac
JetsadaWijit <Ze_ro_owen@hotmail.com>
2024-06-20 11:23:01 +0000

    Fix session

--- routes/about.js
+++ routes/about.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('about', { title: 'MEMO', email: req.session.email});
+    res.render('about', { title: 'MEMO', user: req.session.email});
 });
 
 module.exports = router;
