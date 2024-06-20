5ad562f235debf2c34fd29c95f3c775594657586
JetsadaWijit <Ze_ro_owen@hotmail.com>
2024-06-20 10:19:09 +0000

    Added func readUsersEmail(filePath)

--- routes/utils.js
+++ routes/utils.js
@@ -1,29 +1,29 @@
 const path = require('path');
 
 function serveStaticFiles(router, staticPath, filePaths) {
-  filePaths.forEach((filePath) => {
-    router.get(`/public/${filePath}`, (req, res) => {
-      const fullPath = path.join(staticPath, filePath);
+	filePaths.forEach((filePath) => {
+		router.get(`/public/${filePath}`, (req, res) => {
+			const fullPath = path.join(staticPath, filePath);
 
-      res.sendFile(fullPath, (err) => {
-        if (err) {
-          console.error(err);
-          // Handle errors more informatively (e.g., specific error codes)
-          res.status(err.code || 500).send('Error serving static file'); // Generic error with code if available
-        } else {
-          console.log(`Successfully served static file: ${fullPath}`); // Optional logging for successful requests
-        }
-      });
-    });
-  });
+			res.sendFile(fullPath, (err) => {
+				if (err) {
+					console.error(err);
+					// Handle errors more informatively (e.g., specific error codes)
+					res.status(err.code || 500).send('Error serving static file'); // Generic error with code if available
+				} else {
+					console.log(`Successfully served static file: ${fullPath}`); // Optional logging for successful requests
+				}
+			});
+		});
+	});
 }
 
 function readUsersEmail(filePath) {
-  const data = fs.readFileSync(filePath);
-  return JSON.parse(data);
+	const data = fs.readFileSync(filePath);
+	return JSON.parse(data);
 }
 
 module.exports = {
-    readUsersEmail,
-    serveStaticFiles
+	readUsersEmail,
+	serveStaticFiles
 };
