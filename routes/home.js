0ef74dd89f2e2fc2decfc0ca6cbb7abb963ebadc
JetsadaWijit <Ze_ro_owen@hotmail.com>
2024-06-20 12:40:02 +0000

    Fix var

--- routes/home.js
+++ routes/home.js
@@ -33,14 +33,14 @@ router.get('/', (req, res) => {
 router.post('/register', (req, res) => {
     const { email, password } = req.body;
     const emails = readUsersEmail((filePath));
-    const exists = emails.some((user) => user.email === email);
+    const user = emails.find((user) => user.email === email);
 
-    if (exists) {
+    if (user) {
         res.send('User already exists');
     } else {
         const hashedPassword = bcrypt.hashSync(password, 10);
         writeUsersEmail(path.join(filePath), [...emails, { email, password: hashedPassword }]);
-        req.session.email = email;
+        req.session.user = user;
         res.redirect('/about');
     }
 });
