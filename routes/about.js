d23344f5ede8f14565f3c68ce7cf2aa23c3f2832
JetsadaWijit <Ze_ro_owen@hotmail.com>
2024-06-20 11:19:03 +0000

    Added required argument

--- routes/about.js
+++ routes/about.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('about', { title: 'MEMO' });
+    res.render('about', { title: 'MEMO', email: req.session.email});
 });
 
 module.exports = router;
