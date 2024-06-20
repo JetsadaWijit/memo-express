1db0b30a124938813f3bd1b453d586411304150b
JetsadaWijit <Ze_ro_owen@hotmail.com>
2024-06-20 10:53:40 +0000

    optimize code

--- routes/home.js
+++ routes/home.js
@@ -4,6 +4,8 @@ const bcrypt = require('bcryptjs');
 
 const router = express.Router();
 
+const filePath = path.join(__dirname, '..', 'data', 'users.json');
+
 const { readUsersEmail, writeUsersEmail } = require('./utils');
 
 // utils
@@ -26,14 +28,14 @@ router.get('/', (req, res) => {
 // post register using bcrypt
 router.post('/register', (req, res) => {
     const { email, password } = req.body;
-    const emails = readUsersEmail(path.join(__dirname, '..', 'data', 'users.json'));
+    const emails = readUsersEmail(filePath));
     const exists = emails.some((user) => user.email === email);
 
     if (exists) {
         res.send('User already exists');
     } else {
         const hashedPassword = bcrypt.hashSync(password, 10);
-        writeUsersEmail(path.join(__dirname, '..', 'data', 'users.json'), [...emails, { email, password: hashedPassword }]);
+        writeUsersEmail(path.join(filePath), [...emails, { email, password: hashedPassword }]);
         res.redirect('/about', { title: 'MEMO', email });
     }
 });
