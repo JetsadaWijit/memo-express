6037170f2d134ad32d6de00677ed0fe6f8ef619e
JetsadaWijit <Ze_ro_owen@hotmail.com>
2024-06-20 11:40:58 +0000

    Del un use code

--- routes/about.js
+++ routes/about.js
@@ -4,7 +4,6 @@ const session = require('express-session');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    console.log(req.session.email);
     res.render('about', { title: 'MEMO', user: req.session.email});
 });
 
