4e4dc2c7328f41befea4fa1359e995f9319521b6
JetsadaWijit <Ze_ro_owen@hotmail.com>
2024-06-20 11:55:36 +0000

    Added check session

--- routes/about.js
+++ routes/about.js
@@ -4,6 +4,9 @@ const session = require('express-session');
 const router = express.Router();
 
 router.get('/', (req, res) => {
+    if (!req.session.email) {
+        res.redirect('/');
+    }
     res.render('about', { title: 'MEMO', user: req.session.email});
 });
 
